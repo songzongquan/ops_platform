fd603ecc6cc1431d1ec89e198e1761c581954b52
root <root@localhost.localdomain>
2019-09-22 12:46:20 +0800

    增加了测试用例，完善了DBUtil类

--- dbutil.py
+++ dbutil.py
@@ -1,29 +1,44 @@
-#!/usr/bin/python3
-#encoding:utf8
+﻿#!/usr/bin/python3
+#encoding:utf-8
 
 import pymysql
 
 class DBUtil:
-
-    openConn(self,host,user,passwd,dbname):
-        self.conn = pymysql.connect(host=host,user=user, password=password, database=dbname, charset='utf8')        
+    
+    '''构造方法 '''
+    
+    def __init__(self,host,user,passwd,dbname):
+        self.host = host
+        self.user = user
+        self.password = passwd
+        self.dbname = dbname
+    '''打开连接 '''
+    def openConn(self):
+        self.conn = pymysql.connect(host=self.host,user=self.user, password=self.password, database=self.dbname, charset='utf8')        
         self.cursor = self.conn.cursor()
-
-    closeConn(self):
+    '''关闭连接'''
+    def closeConn(self):
         self.cursor.close()
         self.conn.close()
-
-    query(self,sql):
-        pass
-
-    update(self,sql,params):
+    '''查询  sql为字串，params为参数元组'''
+    def query(self,sql,params):
+        self.openConn()
+        query = (sql)
+        
+        self.cursor.execute(query,params)
+        result = self.cursor.fetchall()
+        
+        self.closeConn()
+        return result
+    '''插入，删除，更新 '''
+    def update(self,sql,params):
         self.openConn()
         query = (sql)
         self.cursor.execute(query,params)
         self.conn.commit()
         self.closeConn()
-
-    batchUpdate(self,sql_params):
+    ''' 批量更新，即一次执行多个语句  ，sql_params为一个list，其元素为dict, dict中，KEY 为“sql”的值为sql语句，KEY为params的值为参数列表元组 '''
+    def batchUpdate(self,sql_params):
         self.openConn()
         for sql_param in sql_params:
             query = (sql_param['sql'])
---
+++ dbutil_test.py
@@ -0,0 +1,70 @@
+﻿#!/usr/bin/python3
+#encoding:utf-8
+
+import unittest
+
+from dbutil import DBUtil
+
+class MyTest(unittest.TestCase):
+    def tearDown(self):
+        print('当前用例执行后清理...')
+              
+        sql = 'delete from vir_host'
+        params = ()
+        self.dbutil.update(sql,params)
+              
+    def setUp(self):
+        print('当前用例执行前初始化...')
+        self.dbutil = DBUtil('localhost','root','','ops_db')
+    @classmethod
+    def tearDownClass(self):
+        print('所有用例执行后的清理...')
+
+    @classmethod
+    def setUpClass(self):
+        print('所有用例执行前的初始...')
+
+    def test_update(self):
+        sql = 'insert into vir_host(ip,purpose,type,state,root_pass,depart,owner,email,phone,memo)  values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
+        params = ('127.0.0.1',u'测试','test','active','123kissme',u'工业互联网','song','songzongquan@bonc.com.cn','18638217761','haha')
+        self.dbutil.update(sql,params)
+        
+        sql = 'select ip,purpose,type,state,root_pass,depart,owner,email,phone,memo from vir_host where ip = %s'
+        params = ('127.0.0.1')
+        result = self.dbutil.query(sql,params)
+        #print(result)
+        
+        self.assertEqual('test',result[0][2])
+
+    def test_batchUpdate(self):
+        
+        sql1 = 'insert into vir_host(ip,purpose,type,state,root_pass,depart,owner,email,phone,memo)  values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
+        params1 = ('127.0.0.1',u'测试1','test1','active','123kissme',u'工业互联网','song','songzongquan@bonc.com.cn','18638217761','haha')
+        sql2 = 'insert into vir_host(ip,purpose,type,state,root_pass,depart,owner,email,phone,memo)  values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
+        params2 = ('127.0.0.2',u'测试2','test2','active','123kissme',u'工业互联网','song','songzongquan@bonc.com.cn','18638217761','haha')     
+        
+        sql_params = []
+        sql_param = {}
+        sql_param['sql'] = sql1
+        sql_param['params'] = (params1)
+        
+        sql_params.append(sql_param)
+        
+        sql_param2 ={}
+        
+        sql_param2['sql'] = sql2
+        sql_param2['params'] = (params2)
+        sql_params.append(sql_param2)
+                       
+        self.dbutil.batchUpdate(sql_params)
+        
+        sql = 'select count(1) from vir_host '
+        params = ()
+        result = self.dbutil.query(sql,params)
+        
+        
+        
+        self.assertEqual(2,result[0][0])
+
+if __name__ == '__main__':
+    unittest.main()
